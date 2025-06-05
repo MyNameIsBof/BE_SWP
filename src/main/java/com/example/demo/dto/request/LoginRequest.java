22106842058ccf9f04e16d7a2702d97f39efa486
huuvinh05926 <huuvinh05926@gmail.com>
2025-06-05 15:53:10 +0700

    update1

--- src/main/java/com/example/demo/dto/request/LoginRequest.java
+++ src/main/java/com/example/demo/dto/request/LoginRequest.java
@@ -5,5 +5,13 @@ import lombok.Data;
 @Data
 public class LoginRequest {
     public String email;
+<<<<<<< main
+=======
+    @NotBlank(message = "Không được để trống")
+//    @Pattern(
+//            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
+//            message = "Mật khẩu phải 8-20 ký tự, có chữ hoa, chữ thường, số và ký tự đặc biệt"
+//    )
+>>>>>>> local
     public String password;
 }
