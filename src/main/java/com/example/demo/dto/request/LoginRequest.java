e081c2f24b3156a843f343d0f7831d1d08c903fa
huuvinh05926 <huuvinh05926@gmail.com>
2025-06-05 16:04:35 +0700

    fix conflict2

--- src/main/java/com/example/demo/dto/request/LoginRequest.java
+++ src/main/java/com/example/demo/dto/request/LoginRequest.java
@@ -10,21 +10,16 @@ public class LoginRequest {
     @Email
     @NotBlank(message = "Không được để trống")
     public String email;
-<<<<<<< HEAD
-<<<<<<< main
-=======
     @NotBlank(message = "Không được để trống")
 //    @Pattern(
 //            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
 //            message = "Mật khẩu phải 8-20 ký tự, có chữ hoa, chữ thường, số và ký tự đặc biệt"
 //    )
->>>>>>> local
-=======
     @NotBlank(message = "Không được để trống")
     @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$",
             message = "Mật khẩu phải 8-20 ký tự, có chữ hoa, chữ thường, số và ký tự đặc biệt"
     )
->>>>>>> 5739803dc40f1a81f287b1403629a4fe6a53de04
+    
     public String password;
 }
